51b1962fba823937c1e2b3a567fd35cd96927fa6
Reza R Ramadhan <reza@contrivent.com>
2018-04-25 12:17:56 +0700

    UPDATE

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 
     Welcome, {{ Auth::getUser()->name }}
     
+    <img src="{{url('assets/images/logo.jpeg')}}" style="display: block;margin: 30px auto;" />
 @endsection
 
 @section('custom_js')
