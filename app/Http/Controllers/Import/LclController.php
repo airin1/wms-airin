f90c6305f5eb8fc1c9180a1237b482b1b1b219bd
Reza R Ramadhan <reza@contrivent.com>
2019-04-02 12:00:35 +0700

    Update

--- app/Http/Controllers/Import/LclController.php
+++ app/Http/Controllers/Import/LclController.php
@@ -1902,6 +1902,10 @@ class LclController extends Controller
 //                            endif;
                             if(in_array($count_ex_header, array(1,2,3))):
                                 continue;
+                            elseif($count_ex_header == 7):
+                                $ex_header_nohbl = isset($ex_header[2]) ? $ex_header[2] : null;
+                                $ex_header_tglhbl = isset($ex_header[4]) ? $ex_header[4] : null;
+                                $ex_header_berat = isset($ex_header[5]) ? $ex_header[5] : null;
                             elseif($count_ex_header == 8):
                                 $ex_header_nohbl = isset($ex_header[3]) ? $ex_header[3] : null;
                                 $ex_header_tglhbl = isset($ex_header[5]) ? $ex_header[5] : null;
@@ -1927,7 +1931,7 @@ class LclController extends Controller
                                 $ex_header_tglhbl = isset($ex_header[10]) ? $ex_header[10] : null;
                                 $ex_header_berat = isset($ex_header[11]) ? $ex_header[11] : null;
                             else:
-//                                return $count_ex_header;
+                                return $count_ex_header;
                                 return back()->with('error', 'Cannot upload TXT file, new flat file detected.')->withInput();
                             endif;
                             
