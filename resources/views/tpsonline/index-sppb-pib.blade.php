7e8c8489f0af76a5cf9f5c027ae11656cd715e44
airin <airin.sekretariat@gmail.com>
2022-12-22 16:31:59 +0700

    update download sppb

--- resources/views/tpsonline/index-sppb-pib.blade.php
+++ resources/views/tpsonline/index-sppb-pib.blade.php
@@ -187,7 +187,11 @@
                                 <label class="col-sm-2 control-label">Tahun</label>
                                 <div class="col-sm-2">
                                     <select class="form-control select2" id="TCONSIGNEE_FK" name="thn_sppb" style="width: 100%;" tabindex="-1" aria-hidden="true" required>
-                                        <option value="2017">2017</option>
+                                        <option value="2013">2013</option>
+										<option value="2014">2014</option>
+										<option value="2015">2015</option>
+									    <option value="2016">2016</option>
+									    <option value="2017">2017</option>
                                         <option value="2018">2018</option>
                                         <option value="2019">2019</option>
                                         <option value="2020">2020</option>
