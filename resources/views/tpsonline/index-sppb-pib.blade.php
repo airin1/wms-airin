9c034699e3cd4da3d464112af81486671c33f592
airin <airin.sekretariat@gmail.com>
2023-01-02 12:10:21 +0700

    update 2023

--- resources/views/tpsonline/index-sppb-pib.blade.php
+++ resources/views/tpsonline/index-sppb-pib.blade.php
@@ -195,8 +195,9 @@
                                         <option value="2018">2018</option>
                                         <option value="2019">2019</option>
                                         <option value="2020">2020</option>
-                                        <option value="2021" >2021</option>
-										 <option value="2022" selected>2022</option>
+                                        <option value="2021">2021</option>
+										<option value="2022">2022</option>
+										<option value="2023" selected>2023</option>
                                     </select>
                                 </div>
                             </div>
