d39067f5236bff8486f436d93b78d47cc7386ce5
airin <airin.sekretariat@gmail.com>
2024-04-23 12:20:10 +0700

    update filter gate in dan release

--- resources/views/import/fcl/index-gatein.blade.php
+++ resources/views/import/fcl/index-gatein.blade.php
@@ -282,6 +282,45 @@
     </div>
     <div class="box-body">
         <div class="row" style="margin-bottom: 30px;">
+		  <div class="row" style="margin-bottom: 30px;margin-right: 0;">
+            <div class="col-md-8">
+                <div class="col-xs-12">Search By Date</div>
+                <div class="col-xs-12">&nbsp;</div>
+                <div class="col-xs-3">
+                    <select class="form-control" id="by" name="by" style="width: 100%;" tabindex="-1" aria-hidden="true">
+                        <option value="TGL_PLP">Tgl. PLP</option>
+                        <option value="TGLMASUK">Tgl. GateIn</option>
+                       
+                    </select>
+                </div>
+                <div class="col-xs-3">
+                    <div class="input-group date">
+                        <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <input type="text" id="startdate" class="form-control pull-right datepicker">
+                    </div>
+                </div>
+                <div class="col-xs-1">
+                    s/d
+                </div>
+                <div class="col-xs-3">
+                    <div class="input-group date">
+                        <div class="input-group-addon">
+                            <i class="fa fa-calendar"></i>
+                        </div>
+                        <input type="text" id="enddate" class="form-control pull-right datepicker">
+                    </div>
+                </div>
+                <div class="col-xs-2">
+                    <button id="searchByDateBtn" class="btn btn-default">Search</button>
+                </div>
+            </div>
+            
+        </div> 
+		
+		
+		
             <div class="col-md-12"> 
                 {{
                     GridRender::setGridId("fclGateinGrid")
@@ -739,6 +778,44 @@
 //        var newDate = new Date(actualDate.getFullYear(), actualDate.getMonth(), actualDate.getDate()+3);
 //        $('#P_TGLKELUAR').datepicker('setDate', newDate );
 //    });
+</script>
+
+<script type="text/javascript">
+    $('.datepicker').datepicker({
+        autoclose: true,
+        todayHighlight: true,
+        format: 'yyyy-mm-dd',
+        zIndex: 99
+    });
+    
+    $('#searchByDateBtn').on("click", function(){
+        var by = $("#by").val();
+        var startdate = $("#startdate").val();
+        var enddate = $("#enddate").val();
+        var string_filters = '';
+        var filters = '{"groupOp":"AND","rules":[{"field":"'+by+'","op":"ge","data":"'+startdate+'"},{"field":"'+by+'","op":"le","data":"'+enddate+'"}]}';
+
+        var current_filters = jQuery("#fclGateinGrid").getGridParam("postData").filters;
+        
+        if (current_filters) {
+            var get_filters = $.parseJSON(current_filters);
+            if (get_filters.rules !== undefined && get_filters.rules.length > 0) {
+
+                var tempData = get_filters.rules;
+                
+                tempData.push( { "field":by,"op":"ge","data":startdate } );
+                tempData.push( { "field":by,"op":"le","data":enddate } );
+                
+                string_filters = JSON.stringify(tempData);
+                
+                filters = '{"groupOp":"AND","rules":'+string_filters+'}';
+            }
+        }
+
+        jQuery("#fclGateinGrid").jqGrid("setGridParam", { postData: {filters} }).trigger("reloadGrid");
+        
+        return false;
+    });
 </script>
 
 @endsection
